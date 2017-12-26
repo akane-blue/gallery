070ebea2f7d1ad42a64e16afa0de4aeee7fa8cbf
orumin <dev@orum.in>
2017-12-26 17:41:00 +0900

    render.py: change file extension for thumbnail

--- render.py
+++ render.py
@@ -3,10 +3,18 @@
 
 GALLERY_JSON = 'gallery.json'
 
+import re
 import json
 from jinja2 import Environment, FileSystemLoader
 
+# Custom filter method
+def regex_replace(s, find, replace):
+    """A non-optimal implementation of a regex filter"""
+    return re.sub(find, replace, s)
+
 env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
+env.filters['regex_replace'] = regex_replace
+
 template = env.get_template('index.html.j2')
 
 akanes=[]
